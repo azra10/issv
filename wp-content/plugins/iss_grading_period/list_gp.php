8d8b2e2c30bd164139a6eb4755de9004c56c1cac
azra10 <azrafatima@Azras-MacBook-Pro.local>
2017-10-24 10:29:34 -0700

    grading period list

---
+++ wp-content/plugins/iss_grading_period/list_gp.php
@@ -0,0 +1,42 @@
+<?php 
+    if (iss_current_user_can_admin()) { 
+         iss_show_heading("Grading Periods", "admin.php?page=new_gp");
+    } else {
+        iss_show_heading("Grading Periods");
+    }    
+    $result_set = ISS_GradingPeriodService::GetGradingPeriods ( );
+?>
+<div>
+    <div class="row">
+        <table class="table table-striped table-responsive table-condensed" id="iss_gp_table">
+            <thead>
+                <tr>
+                    
+                    <th>Registration Year</th>
+                    <th>Grading Period</th>
+                    <th>Start Date</th>
+                    <th>End Date</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($result_set as $row) { ?>
+                <tr>
+                <td><?php echo $row->RegistrationYear; ?></td>
+                <td> <?php echo $row->GradingPeriod ;?> </td>
+                <td> <?php echo $row->StartDate ;?> </td>
+                <td> <?php echo $row->EndDate ;?> </td>
+                <td>
+                <a href="admin.php?page=edit_gp&gid=<?php echo $row->GradingPeriodID ; ?>">
+                    <span style="padding-left: 10px; white-space: nowrap;"> <i class="glyphicon glyphicon-edit"></i> Edit </span>
+                </a>
+                <a href="admin.php?page=delete_gp&gid=<?php echo $row->GradingPeriodID ; ?>">
+                    <span style="padding-left: 10px; white-space: nowrap;"> <i class="glyphicon glyphicon-remove"></i> Delete </span>
+                </a>
+                </td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</div>
